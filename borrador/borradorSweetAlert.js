512cf02a6dee574d79d5204fc992f3c6a6e8ab12
KarinaMendietta <karinamendiettasoto@gmail.com>
2022-03-01 23:12:13 -0500

    dia2 semana6

--- borrador/borradorSweetAlert.js
+++ borrador/borradorSweetAlert.js
@@ -22,7 +22,7 @@ for (let i=0; i<arraysdeobjetos.length;i++);
 console.log(arraysdeobjetos[0].celular);
 
   console.log(arraysdeobjetos[0].celular);
-*/
+
 
 const arrayOptions = [
   {
@@ -36,43 +36,109 @@ const arrayOptions = [
     broccoli: "Broccoli",
     carrot: "Carrot",
   },
- 
 ];
 
 console.log(arrayOptions[0].apples);
 
 const arraysdeobjetos = [
   {
-    nombre:"celular",
-      marca: "Apple",
-      color: "Gris",
-    
+    nombre: "celular",
+    marca: "Apple",
+    color: "Gris",
   },
   {
-    nombre:"laptop",
-      marca: "Lenovo",
-      color: "azul",
-    
+    nombre: "laptop",
+    marca: "Lenovo",
+    color: "azul",
   },
   {
-    nombre:"televisor",
-      marca: "LG",
-      color: "Gris",
-    
+    nombre: "televisor",
+    marca: "LG",
+    color: "Gris",
   },
 ];
 
 /*for (let i=0; i<arraysdeobjetos.length;i++);
 console.log(arraysdeobjetos[0].nombre);*/
-
-
+/*
 arraysdeobjetos.map((arraysdeobjeto) => {
-    console.log(arraysdeobjeto.marca);
+  console.log(arraysdeobjeto.marca);
 });
 
 //meter marcas en el array vacio
-arrayMarcas =[]
+arrayMarcas = [];
 
 arraysdeobjetos.map((arraysdeobjeto) => {
-    return(arraysdeobjeto.marca);
+  return arraysdeobjeto.marca;
 });
+/* if (edad>=18) {
+  return("Es mayor de edad");
+} else {
+  return("Es menor de edad");
+};
+};
+
+console.log(esMayorDeEdad(20)); */
+/*
+const esMayorDeEdad = function (edad) {
+  if (edad >= 18) {
+    return "mayor de edad";
+  }
+  return "menor de edad";
+};
+
+console.log(esMayorDeEdad(10));
+
+const sumar = function (a,b) {;
+  const calc = +a + +b;
+  return calc
+};
+
+console.log(sumar(2,5));
+*/
+/*
+while (alumnos){
+  let contador = 0;
+  while (contador<alumnos.length){
+      contador++;
+  }
+}
+
+const alumnos = ["maria","pepe","mario","cesar"];
+ for (let i =0;i<alumnos.length;i++){
+ console.log(alumnos[0]);
+}
+
+const alumnos2 = [
+  {
+    nombre:"marta",
+    edad:21,
+  },
+  {
+    nombre: "juan",
+    edad: 30,
+  },
+  {
+    nombre:"luis",
+    edad:25,
+  },
+];
+for(let obj=0;obj<alumnos2.length;obj++){
+  console.log(alumnos2);
+}
+//while
+*/
+ const imprimirCincoVeces =(palabra) =>{
+   let contador = 0;
+   while (contador <5) {
+     console.log(palabra);
+     contador++
+   }
+ }
+imprimirCincoVeces("kari");
+
+const hallarImpares = (numero) => {
+  if(numero<=0){
+    return "Solo numeros positivos"
+  }
+};
